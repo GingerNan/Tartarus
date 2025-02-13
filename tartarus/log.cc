d82b528910edc7c4d2396708faa7895c39aa6a43
Yolo <ginger_nan@163.com>
2025-02-13 18:45:53 +0800

    [Add] 新增日志文件

---
+++ tartarus/log.cc
@@ -0,0 +1,153 @@
+#include "log.h"
+
+namespace Tartarus
+{
+
+Logger::Logger(const std::string& name)
+{
+
+}
+
+void Logger::addAppender(LogAppender::ptr appender)
+{
+    m_appenders.push_back(appender);
+}
+
+void Logger::delAppender(LogAppender::ptr appender)
+{
+    for(auto it = m_appenders.begin(); it != m_appenders.end(); ++it)
+    {
+        if (*it == appender)
+        {
+            m_appenders.erase(it);
+            break;
+        }
+    }
+}
+
+void Logger::log(LogLevel::Level level, LogEvent::ptr event)
+{
+    if (level >= m_level)
+    {
+        for (auto& i : m_appenders)
+        {
+            i->log(level, event);
+        }
+    }
+}
+
+void Logger::debug(LogEvent::ptr event)
+{
+    log(LogLevel::DEBUG, event);
+}
+    
+void Logger::info(LogEvent::ptr event)
+{
+    log(LogLevel::INFO, event);
+}
+
+void Logger::warn(LogEvent::ptr event)
+{
+    log(LogLevel::WARN, event);
+}
+
+void Logger::error(LogEvent::ptr event)
+{
+    log(LogLevel::ERROR, event);
+}
+
+void Logger::fatal(LogEvent::ptr event)
+{
+    log(LogLevel::FATAL, event);
+}
+
+FileLogAppender::FileLogAppender(const std::string& filename)
+    :m_filename(filename)
+{
+}
+
+bool FileLogAppender::reopen()
+{
+    if (m_filestream)
+    {
+        m_filestream.close();
+    }
+
+    m_filestream.open(m_filename);
+    return !!m_filestream;
+}
+
+void FileLogAppender::log(LogLevel::Level level, LogEvent::ptr event)
+{
+    if (level >= m_level)
+    {
+        m_filestream << m_formatter->format(event);
+    }
+}
+
+void StdoutLogAppender::log(LogLevel::Level level, LogEvent::ptr event)
+{
+    if (m_level >= m_level)
+    {
+        std::cout << m_formatter->format(event);
+    }
+}
+
+LogFormatter::LogFormatter(const std::string& pattern)
+    :m_pattern(pattern)
+{
+}
+
+std::string LogFormatter::format(LogEvent::ptr event)
+{
+    std::stringstream ss;
+    for(auto& i : m_items)
+    {
+        i->format(ss, event);
+    }   
+    return ss.str();
+}
+
+//%xxxx %xxx{xxx} %%
+void LogFormatter::init()
+{
+    //str, format, type
+    std::vector<std::tuple<std::string, std::string, int>> vec;
+    std::string str;
+    for (size_t i = 0; i < m_pattern.size(); ++i)
+    {
+        if (m_pattern[i] != '%')
+        {
+            str.append(1, m_pattern[i]);
+            continue;
+        }
+
+        size_t n = i + 1;
+        int fmt_status = 0;
+
+        std::string str;
+        std::string fmt;
+        while (n < m_pattern.size())
+        {
+            if (isspace(m_pattern[n]))
+            {
+                break;
+            }
+
+            if (fmt_status == 0)
+            {
+                if (m_pattern[n] == '{')
+                {
+                    str = m_pattern.substr(i + 1, n - i);
+                    fmt_status = 1; //解析格式
+                    ++n;
+                    continue;
+                }
+            }
+        }
+        
+    }    
+}
+
+
+}
